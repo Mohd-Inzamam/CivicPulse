36134c5f2365a7a5ac1e2a3897a13c306ee1a8b7
Inzamam Sheikh <inzamam2812@gmail.com>
2026-06-04 20:22:38 +0530

    Added explicit localhost URL for better working in development environment

--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,10 @@ const app = express()
 
 app.use(
   cors({
-    origin: [process.env.CLIENT_URL], // ✅ explicitly specify allowed origins
+    origin: [
+      "http://localhost:5173",
+      process.env.CLIENT_URL
+    ], // ✅ explicitly specify allowed origins
     credentials: true,                // ✅ allow cookies / auth headers
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
